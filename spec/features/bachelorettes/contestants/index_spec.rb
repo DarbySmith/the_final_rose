365c943a186a09ee57812eaf5933607dcc13ee73
Darby Smith <newman.darby2@gmail.com>
2022-11-14 09:11:30 -0500

    User story 2/6

---
+++ spec/features/bachelorettes/contestants/index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'bachelorette show page' do
+  before(:each) do
+    @bach_1 = Bachelorette.create!(name: "Hannah Brown", season_number: 11, description: "Most dramatic season yet")
+    @bach_2 = Bachelorette.create!(name: "Clare Crawley", season_number: 13, description: "Truly the most dramatic season yet")
+
+    @contestant_1 = @bach_1.contestants.create!(name: "Peter Weber", age: 31, hometown: "Los Angeles, CA")
+    @contestant_2 = @bach_1.contestants.create!(name: "Tyler Cameron", age: 29, hometown: "Jupiter, FL")
+    @contestant_3 = @bach_2.contestants.create!(name: "Dale Moss", age: 31, hometown: "New York, NY")
+  end
+
+  it 'shows contestants for a bachelorette' do
+    visit bachelorette_contestants_path(@bach_1)
+
+    within("#contestant-#{@contestant_1.id}") do
+      expect(page).to have_content(@contestant_1.name)
+      expect(page).to have_content(@contestant_1.age)
+      expect(page).to have_content(@contestant_1.hometown)
+
+      expect(page).to_not have_content(@contestant_3.name)
+      expect(page).to_not have_content(@contestant_3.hometown)
+    end
+
+    within("#contestant-#{@contestant_2.id}") do
+      expect(page).to have_content(@contestant_2.name)
+      expect(page).to have_content(@contestant_2.age)
+      expect(page).to have_content(@contestant_2.hometown)
+
+      expect(page).to_not have_content(@contestant_3.name)
+      expect(page).to_not have_content(@contestant_3.age)
+      expect(page).to_not have_content(@contestant_3.hometown)
+    end
+  end
+end
